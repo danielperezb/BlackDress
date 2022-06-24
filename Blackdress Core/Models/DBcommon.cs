6ed1f4040f338a1a80db1e06fd1aed5e83f2695a
NicoLastra <nicolas.lastra.olguin@gmail.com>
2022-06-24 12:05:17 -0400

    no se

---
+++ Blackdress Core/Models/DBcommon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Blackdress_Core.Models
+{
+    public class DBcommon
+    {
+        private static string Conn = @"server=127.0.0.1;user id=root;persistsecurityinfo=True;database=blackdress";
+
+        public static IDbConnection Conexion()
+        {
+            return new MySqlConnection(Conn);
+        }
+    }
+}
